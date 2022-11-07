76bc820233279e40606978c668ecfde916e70df2
Gianalfonso Gardini <gianalfonso@digitware.it>
2022-11-07 21:59:34 +0100

    render helper

---
+++ MySongsWebApp/MySongsWebApp/Helpers/SongHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Html;
+using MySongsWebApp.Models;
+
+namespace MySongsWebApp.Helpers
+{
+    public static class SongHelper
+    {
+    
+    public static IHtmlContent SongRenderer(SongModel song)
+        {
+            string html;
+            if (song.Authors.Count > 0)
+            {
+                var authors = String.Join(",", song.Authors);
+                html = $"<li class=\"song\">{@song.Id} - {song.Title} (Authors: {authors})</li>";
+            } else
+            {
+                html = $"<li class=\"song\">{@song.Id} - {song.Title}";
+            }
+
+            return new HtmlString(html);
+        }
+    }
+}
