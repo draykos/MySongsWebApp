e8369d5f1e21967c3cc1e0f717e388b08d0b0cf3
Gianalfonso Gardini <gianalfonso@digitware.it>
2022-11-07 17:05:40 +0100

    add a new controller

---
+++ MySongsWebApp/MySongsWebApp/Controllers/SettingsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MySongsWebApp.Controllers
+{
+    public class SettingsController : Controller
+    {
+        private readonly ILogger<SettingsController> logger;
+
+        public SettingsController(ILogger<SettingsController> logger)
+        {
+            this.logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            logger.LogInformation("Hello, I'm settings");
+            return View();
+        }
+    }
+}
