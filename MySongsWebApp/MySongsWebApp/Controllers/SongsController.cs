0decd1cde6e300e0ca5b577e135360ece4ee27e7
Gianalfonso Gardini <gianalfonso@digitware.it>
2022-11-07 19:35:03 +0100

    added a model

--- MySongsWebApp/MySongsWebApp/Controllers/SongsController.cs
+++ MySongsWebApp/MySongsWebApp/Controllers/SongsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MySongsWebApp.Models;
 
 namespace MySongsWebApp.Controllers
 {
@@ -14,6 +15,19 @@ namespace MySongsWebApp.Controllers
 
         public IActionResult Index()
         {
+
+            var songs = new List<SongModel>{
+                new SongModel{Id=1, Title="Wish You Were Here", Authors=new List<string>{"Pink Floyd"} },
+                new SongModel{Id=2, Title="Dark Side of The Moon", Authors=new List<string>{"Pink Floyd"} },
+                new SongModel{Id=3, Title="The Sound of Silence", Authors=new List<string>{"Paul Simon", "Arl Garfunkel"} },
+                new SongModel{Id=100, Title="Fear of the Dark", Authors=new List<string>{"Iron Maiden"} },
+                new SongModel{Id=102, Title="Run to the Hills", Authors=new List<string>{"Iron Maiden"} },
+                new SongModel{Id=201, Title="Nothing Else Matters", Authors=new List<string>{"Metallica"} },
+                new SongModel{Id=305, Title="Light My Love", Authors=new List<string>{"Greta Van Fleet"} },
+            };
+            ViewData["Title"] = "Songs";
+            ViewData["Songs"] = songs;
+
             return View();
         }
     }
