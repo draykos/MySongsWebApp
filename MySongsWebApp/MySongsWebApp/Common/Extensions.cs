60b8c796d8399f7c08bdb34f12e0f8064f28419c
Gianalfonso Gardini <gianalfonso@digitware.it>
2022-11-07 20:22:16 +0100

    added simple extension

---
+++ MySongsWebApp/MySongsWebApp/Common/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using MySongsWebApp.Models;
+
+namespace MySongsWebApp.Extensions
+{
+    public static class Extensions
+    {
+        public static List<SongModel> SortByName(this List<SongModel> input)
+        {
+            var sorted = input.OrderBy(s => s.Title).ToList();
+            return sorted;
+        }
+    }
+}
