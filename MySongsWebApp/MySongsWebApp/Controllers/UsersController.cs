6001dfb629ab277ec35a87dbdb76237f11cddc16
Gianalfonso Gardini <gianalfonso@digitware.it>
2022-11-23 23:50:50 +0100

    add remove from role

--- MySongsWebApp/MySongsWebApp/Controllers/UsersController.cs
+++ MySongsWebApp/MySongsWebApp/Controllers/UsersController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using MySongs.DAL.Models;
 using MySongs.DAL.Students;
 using MySongsWebApp.Models;
 
@@ -11,13 +10,85 @@ public class UsersController : Controller
     private readonly ILogger<SettingsController> logger;
     private readonly UserManager<ApplicationUser> userManager;
     private readonly SignInManager<ApplicationUser> signinManager;
+    private readonly RoleManager<IdentityRole> roleManager;
 
-    public UsersController(ILogger<SettingsController> logger, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signinManager)
+    public UsersController(ILogger<SettingsController> logger, 
+        UserManager<ApplicationUser> userManager, 
+        SignInManager<ApplicationUser> signinManager,
+        RoleManager<IdentityRole> roleManager)
     {
         this.logger = logger;
         this.userManager = userManager;
         this.signinManager = signinManager;
+        this.roleManager = roleManager;
     }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var users = userManager.Users.ToList();
+        return View(users);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Details(string id)
+    {
+        var user = userManager.Users.FirstOrDefault(r => r.UserName == id);
+        if(user != null)
+        {
+            var roles = await userManager.GetRolesAsync(user);
+            ViewData["UserRoles"] = roles;
+        }
+
+        ViewData["Roles"] = roleManager.Roles.ToList();
+        return View(user);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> AddAdminRole(string id)
+    {
+        var user = userManager.Users.FirstOrDefault(r => r.UserName == id);
+        if (user != null)
+        {
+            var result = await userManager.AddToRoleAsync(user, "Admin");
+        }
+
+        return RedirectToAction("Details", new { Id = id });
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> RemoveAdminRole(string id)
+    {
+        var user = userManager.Users.FirstOrDefault(r => r.UserName == id);
+        if (user != null)
+        {
+            var result = await userManager.RemoveFromRoleAsync(user, "Admin");
+        }
+
+        return RedirectToAction("Details", new { Id = id });
+    }
+
+
+
+    [HttpGet]
+    public async Task<IActionResult> Delete(string id)
+    {
+        var user = userManager.Users.FirstOrDefault(r => r.UserName == id);
+        if (user != null)
+        {
+            var result = await userManager.DeleteAsync(user);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+
 
     [HttpGet]
     public IActionResult Create()
