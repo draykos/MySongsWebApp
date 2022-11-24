abb1caaa04708d49cbb06509b290838c36b87165
Gianalfonso Gardini <gianalfonso@digitware.it>
2022-11-24 01:08:33 +0100

    generate jwt token

---
+++ MySongsWebApp/MySongsApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,73 @@
+using Flurl.Util;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using MySongs.DAL.Models;
+using MySongs.DAL.Students;
+using MySongsApi.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MySongsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+        private readonly SignInManager<ApplicationUser> signinManager;
+
+        public AuthenticationController(IConfiguration configuration, SignInManager<ApplicationUser> signinManager)
+        {
+            this.configuration = configuration;
+            this.signinManager = signinManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            if (model is null)
+            {
+                return BadRequest("Invalid user request!!!");
+            }
+
+            var result = await signinManager.PasswordSignInAsync(model.UserName, model.Password, false, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                var user = await signinManager.UserManager.FindByNameAsync(model.UserName);
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetSection("JWT:Secret").Value));
+                var issuer = configuration.GetSection("JWT:Issuer").Value ?? String.Empty;
+                var audience = configuration.GetSection("JWT:Audience").Value ?? String.Empty;
+
+                var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, user.Id),
+                    }),
+                    Expires = DateTime.UtcNow.AddMinutes(10),
+                    Issuer = issuer,
+                    Audience = audience,
+                    SigningCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256Signature)
+                };
+
+                tokenDescriptor.Claims = new Dictionary<string, object>();
+                tokenDescriptor.Claims.Add("key1", "my claim1");
+                tokenDescriptor.Claims.Add("key2", "my claim2");
+                tokenDescriptor.Claims.Add("key3", "my claim3");
+
+
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+                return Ok(new JWTTokenResponse { Token = tokenString });
+            }
+
+            return Unauthorized();
+        }
+    }
+}
---
+++ MySongsWebApp/MySongsApi/Models/JWTTokenResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace MySongsApi.Models
+{
+    public class JWTTokenResponse
+    {
+        public string? Token { get; set; }
+    }
+}
